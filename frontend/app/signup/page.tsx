eb86c7636689185efa1509cf48983f43590dde2c
Hajp <sklawik@icloud.com>
2026-03-03 22:23:31 +0100

    frontend for signup page

--- frontend/app/signup/page.tsx
+++ frontend/app/signup/page.tsx
@@ -5,8 +5,10 @@ export default function Home() {
   return (
     <div className="flex h-full  grow items-center justify-center  font-sans ">
       <main className="flex flex-col grow justify-center items-center">
+        
+
         <form className="bg-zinc-100 dark:bg-black p-4 rounded-lg sm:w-96 h-96 grow flex flex-col justify-between  gap-2  ">
-          <div className="mb-auto p-1 text-md">Login to the service</div>
+          <div className="mb-auto p-1 text-md">Create new account</div>
           <label>Username</label>
           <input
             placeholder="JohnDoe"
@@ -15,14 +17,30 @@ export default function Home() {
           />
           <label>Password</label>
           <input
-          placeholder="***********"
+            placeholder="***********"
+            className="bg-zinc-200 dark:bg-neutral-950 p-1"
+            type="password"
+          />
+          <label>Confirm Password</label>
+          <input
+            placeholder="***********"
             className="bg-zinc-200 dark:bg-neutral-950 p-1"
             type="password"
           />
           <div className="flex flex-row gap-1">
-            <button type="submit" className="bg-white hover:opacity-85 cursor-pointer dark:border-0 border border-gray-800 px-4 py-1 rounded-md text-black ml-auto ">Sign in</button>
+            <button
+              type="submit"
+              className="bg-white hover:opacity-85 cursor-pointer dark:border-0 border border-gray-800 px-4 py-1 rounded-md text-black ml-auto "
+            >
+              Sign up
+            </button>
           </div>
-          <Link href="/signup" className="underline mt-auto dark:text-slate-200 text-slate-800">I don't have an account</Link>
+          <Link
+            href="/"
+            className="underline mt-auto dark:text-slate-200 text-slate-800"
+          >
+            Switch to login page
+          </Link>
         </form>
       </main>
     </div>
