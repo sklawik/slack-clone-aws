98d6befbdc1cf35add98d7e347701f90dd77bd41
Hajp <sklawik@icloud.com>
2026-03-03 22:35:59 +0100

    padding for layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -28,9 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col h-svh w-full `}
       >
-        <div className="sm:ml-96 sm:mr-96 p-2 text-xl">Slack Clone AWS</div>
+        <div className="sm:ml-96 sm:mr-96 p-4 text-xl">Slack Clone AWS</div>
         <div className="sm:ml-96 sm:mr-96 grow ">{children}</div>
-        <div className="sm:ml-96 sm:mr-96 flex flex-col ">
+        <div className="sm:ml-96 sm:mr-96 flex flex-col p-4 ">
           <div>Slack Clone using NextJS, NodeJS and AWS technologies.</div>
           <Link className="underline px-4 py-1 " href="https://github.com/sklawik/slack-clone-aws">Github</Link>
         </div>
