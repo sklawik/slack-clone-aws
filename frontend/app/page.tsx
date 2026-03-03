a934c197fed8e0e057919a339997ec36253604ed
Hajp <sklawik@icloud.com>
2026-03-03 22:17:23 +0100

    slight ux changes

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <div className="flex h-full  grow items-center justify-center  font-sans ">
       <main className="flex flex-col grow justify-center items-center">
-        <form className="bg-zinc-100 dark:bg-black p-4 rounded-lg sm:w-96 h-96 grow flex flex-col gap-2  ">
-          Login to the service
+        <form className="bg-zinc-100 dark:bg-black p-4 rounded-lg sm:w-96 h-96 grow flex flex-col justify-between  gap-2  ">
+          <div className="mb-auto p-1 text-md">Login to the service</div>
           <label>Username</label>
           <input
             placeholder="JohnDoe"
@@ -20,7 +20,7 @@ export default function Home() {
             type="password"
           />
           <div className="flex flex-row gap-1">
-            <button type="submit" className="bg-white dark:border-0 border border-gray-800 px-4 py-1 rounded-md text-black ml-auto ">Sign in</button>
+            <button type="submit" className="bg-white hover:opacity-85 cursor-pointer dark:border-0 border border-gray-800 px-4 py-1 rounded-md text-black ml-auto ">Sign in</button>
           </div>
           <Link href="/register" className="underline mt-auto dark:text-slate-200 text-slate-800">I don't have an account</Link>
         </form>
